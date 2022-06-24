c58ddc7952bda39fe629a453bfa0a336dc6e1514
Vero2112 <verorodriguezruiz21@gmail.com>
2022-06-24 16:29:33 +0000

    primer commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planet, Character, Favorite
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,160 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+# INICIO PLANETS
+@app.route('/planet', methods=['GET'])
+def get_planet():
+
+    planets = Planet.query.all()
+    all_planets = list(map(lambda planet: planet.serialize(), planets))
+
+    return jsonify(all_planets), 200
+
+@app.route('/planet', methods=['POST'])
+def create_planet():
+
+    body = request.get_json()
+    print(body)
+    planet = Planet(name=body["name"], diameter=body["diameter"], rotation=body["rotation"], translation=body["translation"], gravity=body["gravity"])
+    db.session.add(planet)
+    db.session.commit()
+
+    return jsonify(planet.serialize()), 201
+
+@app.route('/planet/<int:planet_id>', methods=['PUT'])
+def refresh_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    # if task is None:
+    #     raise APIException("Tarea no encontrada", 404)
+    body = request.get_json()
+    # if not ("done" in body):
+    #     raise APIException("Tarea no encontrada", 404)
+    # planet = Planet(name=body["name"], diameter=body["diameter"], rotation=body["rotation"], translation=body["translation"], gravity=body["gravity"])
+
+    planet.name = body["name"]
+    planet.diameter = body["diameter"]
+    planet.rotation = body["rotation"]
+    planet.translation = body["translation"]
+    planet.gravity = body["gravity"]
+
+    db.session.commit()
+    return jsonify(planet.serialize())
+
+@app.route('/planet/<int:planet_id>', methods=['GET'])
+def get_detail_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    # if task is None:
+    #     raise APIException("Tarea no encontrada", 404)
+    return jsonify(planet.serialize())
+
+@app.route('/planet/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    # if task is None:
+    #     raise APIException("Tarea no encontrada", 404)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify(planet.serialize())
+# FIN PLANETS
+
+# INICIO PERSONAJES
+@app.route('/character', methods=['GET'])
+def get_characters():
+
+    characters = Character.query.all()
+    all_characters = list(map(lambda character: character.serialize(), characters))
+
+    return jsonify(all_characters), 200
+
+@app.route('/character', methods=['POST'])
+def create_character():
+
+    body = request.get_json()
+    print(body)
+    character = Character(name=body["name"], eye_color=body["eye_color"], birthday_date=body["birthday_date"], gender=body["gender"], height=body["height"])
+    db.session.add(character)
+    db.session.commit()
+
+    return jsonify(character.serialize()), 201
+
+@app.route('/character/<int:character_id>', methods=['PUT'])
+def refresh_character(character_id):
+    character = Character.query.get(character_id)
+    # if task is None:
+    #     raise APIException("Tarea no encontrada", 404)
+    body = request.get_json()
+    # if not ("done" in body):
+    #     raise APIException("Tarea no encontrada", 404)
+    character = Character(name=body["name"], eye_color=body["eye_color"], birthday_date=body["birthday_date"], gender=body["gender"], height=body["height"])
+    db.session.commit()
+    return jsonify(character.serialize()), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_detail_character(character_id):
+    character = Character.query.get(character_id)
+    # if task is None:
+    #     raise APIException("Tarea no encontrada", 404)
+    return jsonify(character.serialize()),200
+
+@app.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character(character_id):
+    character = Character.query.get(character_id)
+    # if task is None:
+    #     raise APIException("Tarea no encontrada", 404)
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify(character.serialize()),200
+# # FIN PERSONAJES
+
+# INICIO USUARIOs
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users = User.query.all()
+    all_users = list(map(lambda user: user.serialize(), users))
 
-    return jsonify(response_body), 200
+    return jsonify(all_users), 200
+
+@app.route('/user/register', methods=['POST'])
+def create_user():
+   
+    body = request.get_json()
+    print(body)
+    user = User(email=body["email"], is_active=True, password="****")
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 201
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        raise APIException("Usuario no encontrado", 404)
+    return jsonify(user.serialize()), 200
+# FIN USUARIOS
+
+# INICIO FAVORITOS
+# Obtener favoritos
+@app.route('/user/<int:user_id>/favorites', methods=['GET'])
+def get_user_favorites(user_id):
+    favorites = Favorite.query.all()
+    all_favorites = list(map(lambda favorite: favorite.serialize(), favorites))
+
+    return jsonify(all_favorites), 200
+
+# Añadir favoritos
+@app.route('/user/<int:user_id>/favorites/planet', methods=['POST'])
+def add_user_favorites(user_id):
+body = request.get_json()
+    print("favoritos body")
+    print(body)
+    favorite_planet = Favorite(planet_id=body["planet_id"], user_id=body["user_id"])
+    db.session.add(favorite_planet)
+    db.session.commit()
+
+    return jsonify(favorite_planet.serialize()), 201
+# FIN FAVORITOS
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,76 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    diameter= db.Column(db.Integer)
+    rotation= db.Column(db.Integer)
+    translation= db.Column(db.Integer)
+    gravity=db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Planet %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "rotation": self.rotation,
+            "translation": self.translation,
+            "gravity": self.gravity
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    eye_color= db.Column(db.String(10))
+    birthday_date= db.Column(db.String(20))
+    gender= db.Column(db.String(10))
+    height=db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Character %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "eye_color": self.eye_color,
+            "birthday_date": self.birthday_date,
+            "gender": self.gender,
+            "height": self.height
+            # do not serialize the password, its a security breach
+        }
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    planet = db.relationship(Planet)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    character = db.relationship(Character)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+
+    def __repr__(self):
+        return '<Favorite %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "planet_id":self.planet_id,
+            "character_id":self.character_id,
+            "user_id":self.user_id
             # do not serialize the password, its a security breach
         }
